3cb6aa87626bafebd978e4ecf8b4a02c77d085b8
konradbukanski <konradbukanski@gmail.com>
2019-06-10 10:38:20 +0200

    description up

--- script.js
+++ script.js
@@ -4,30 +4,37 @@ const Dethleffs = document.querySelector(".ofert__choose--Dethleffs");
 const Volkswagen = document.querySelector(".ofert__choose--Volkswagen");
 
 Carthago.addEventListener("click", () => {
-  console.log("siema");
   document
     .querySelectorAll(".ofert")
     .forEach(item => item.classList.remove("display"));
   document.querySelector(".Carthag").classList.add("display");
+  Carthago.classList.add(".border");
 });
 Adria.addEventListener("click", () => {
-  console.log("siema");
   document
     .querySelectorAll(".ofert")
     .forEach(item => item.classList.remove("display"));
   document.querySelector(".Adria").classList.add("display");
+  Adria.classList.add(".border");
 });
 Dethleffs.addEventListener("click", () => {
-  console.log("siema");
   document
     .querySelectorAll(".ofert")
     .forEach(item => item.classList.remove("display"));
   document.querySelector(".Dethleffs").classList.add("display");
+  Carthago.classList.add(".border");
 });
 Volkswagen.addEventListener("click", () => {
-  console.log("siema");
   document
     .querySelectorAll(".ofert")
     .forEach(item => item.classList.remove("display"));
   document.querySelector(".Caravelle").classList.add("display");
+  Volkswagen.classList.add(".border");
+});
+
+document.addEventListener("scroll", () => {
+  if (document.scrollTop > 50) {
+    console.log("asdsad");
+    document.querySelector(".menu").className.add("hide");
+  }
 });
